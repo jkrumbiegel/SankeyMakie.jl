eb5af18979d195a7c961d974e1d3037b2be037e6
Simon Steiger <106233551+simonsteiger@users.noreply.github.com>
2025-03-21 09:20:31 +0100

    document `nodecolor` arg (#14)
    * document `nodecolor` arg
    
    * use `target` over `destination`

--- src/SankeyMakie.jl
+++ src/SankeyMakie.jl
@@ -10,12 +10,13 @@ export sankey, sankey!
 """
     sankey(connections; kwargs...)
 
-Plots a sankey diagram from the `(source, destination, weight)` entries in `connections`.
+Plots a sankey diagram from the `(source, target, weight)` entries in `connections`.
 
 Specific attributes to `sankey` are:
 - `compact = true`: Reduces the amount of vertical space between nodes in each layer.
 - `fontsize = theme(scene, :fontsize)`: Sets the font size of the node labels.
 - `nodelabels = nothing`: Places labels under the nodes with the corresponding indices.
+- `nodecolor = :gray30`: Sets a color for each node or all nodes if only one color is provided.
 - `linkcolor = (:gray30, 0.2)`: Sets a color for each link or all links if only one color is provided.
 - `forceorder = Pair{Int,Int}[]`: Changes the order of nodes in the same layer(s). Can be `[6 => 1]` (node 6 before 1), or `:reverse` (reverse within all layers).
 
